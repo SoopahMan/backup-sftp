0751aea97fd4154de996b9f5be759ded67fa5ffe
SoopahMan <raviafiansyah22@gmail.com>
2025-04-29 11:07:27 +0700

    update manifest

--- __manifest__.py
+++ __manifest__.py
@@ -30,9 +30,9 @@
     'data': [
         'security/user_groups.xml',
         'security/ir.model.access.csv',
+        'views/backup_view_menu.xml',
         'views/backup_view.xml',
         'data/backup_data.xml',
         'views/backup_scp_view.xml',
-        'views/backup_view_menu.xml',
     ],
 }
