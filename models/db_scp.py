53f7da046dea1b98bc7a5efe12e0946df9d01e7c
SoopahMan <raviafiansyah22@gmail.com>
2025-05-05 09:24:58 +0700

    Update db_scp.py

--- models/db_scp.py
+++ models/db_scp.py
@@ -28,11 +28,22 @@ class DbBackupSCPWizard(models.TransientModel):
             file_path = os.path.join(backup.folder, backup.file_name)
             if not os.path.exists(file_path):
                 raise UserError(f"File tidak ditemukan: {file_path}")
+
             try:
-                subprocess.run([
-                    'scp', '-i', self.scp_private_key,
+                result = subprocess.run([
+                    'scp',
+                    '-i', self.scp_private_key,
+                    '-o', 'StrictHostKeyChecking=no',
                     file_path,
                     f"{self.scp_user}@{self.scp_host}:{self.scp_path}"
-                ], check=True)
+                ], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
             except subprocess.CalledProcessError as e:
-                raise UserError(f"Gagal mengirim file: {e}")
+                raise UserError(f"""
+                    Gagal mengirim file:
+                    Perintah: {e.cmd}
+                    Kode Keluar: {e.returncode}
+                    STDOUT: {e.stdout.decode('utf-8')}
+                    STDERR: {e.stderr.decode('utf-8')}
+                """)
+
